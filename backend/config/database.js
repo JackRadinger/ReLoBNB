056cf9e54787cccb19f4fc48128fbb82b94fcd21
Jack Radinger <jackradinger98@gmail.com>
2021-04-30 16:15:53 -0500

    heroku

--- backend/config/database.js
+++ backend/config/database.js
@@ -16,7 +16,7 @@ module.exports = {
     seederStorage: 'sequelize',
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'postgres://vlxszwvueejeqs:fce822ec25f91f220b6d68a4feaf634a5ebae5e28a527cdacf6a0777f84953e1@ec2-54-87-112-29.compute-1.amazonaws.com:5432/d8r5a1vdtibv1k',
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
