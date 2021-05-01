de16cedc2499fe4cdd065b895ccab76a2b5a5d7d
Jack Radinger <jackradinger98@gmail.com>
2021-05-01 01:36:00 -0500

    changes

--- frontend/src/components/SignUpFormPage/index.js
+++ frontend/src/components/SignUpFormPage/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
+import './SignUp.css'
 
 function SignupFormPage() {
   const dispatch = useDispatch();
@@ -31,90 +32,95 @@ function SignupFormPage() {
   };
 
   return (
-      <div className='form__container'>
-        <form onSubmit={handleSubmit} className='login-form'>
-        <ul>
-            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-        </ul>
-        <div>
-            <label>
-                First Name
-            </label>
-            <input
-                className='first-name-input'
-                type="text"
-                value={firstName}
-                placeholder='First Name'
-                onChange={(e) => setFirstName(e.target.value)}
-                required
-            />
-        </div>
-        <div>
-            <label>
-                Last Name
-            </label>
-            <input
-                className='last-name-input'
-                type="text"
-                value={lastName}
-                placeholder='Last Name'
-                onChange={(e) => setLastName(e.target.value)}
-                required
-            />
-        </div>
-        <div>
-            <label>
-                Email
-            </label>
-        </div>
-        <div>
-            <input
-                className='email-input'
-                type="text"
-                value={email}
-                placeholder='Email'
-                onChange={(e) => setEmail(e.target.value)}
-                required
-            />
-        </div>
-        <div>
-            <label>
-                Username
+      <div className='sign-up-page'>
+        <div className='form__container'>
+            <form onSubmit={handleSubmit} className='login-form'>
+            <ul>
+                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+            </ul>
+            <div className='first-name-input form-input'>
+                <label>
+                    First Name
+                </label>
                 <input
-                type="text"
-                value={username}
-                placeholder='Username'
-                onChange={(e) => setUsername(e.target.value)}
-                required
+                    className='first-name-input form-input'
+                    type="text"
+                    value={firstName}
+                    placeholder='First Name'
+                    onChange={(e) => setFirstName(e.target.value)}
+                    required
                 />
-            </label>
-        </div>
-        <div>
-            <label>
-                Password
+            </div>
+            <div className='last-name-input form-input'>
+                <label>
+                    Last Name
+                </label>
                 <input
-                type="password"
-                value={password}
-                placeholder='Password'
-                onChange={(e) => setPassword(e.target.value)}
-                required
+                    className='last-name-input form-input'
+                    type="text"
+                    value={lastName}
+                    placeholder='Last Name'
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
                 />
-            </label>
-        </div>
-        <div>
-            <label>
-                Confirm Password
+            </div>
+            <div className='email-input form-input'>
+                <label>
+                    Email
+                </label>
+            </div>
+            <div className='form-input'>
                 <input
-                type="password"
-                value={confirmPassword}
-                placeholder='Confirm Password'
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
+                    className='email-input form-input'
+                    type="text"
+                    value={email}
+                    placeholder='Email'
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
                 />
-            </label>
+            </div>
+            <div className='username-input form-input'>
+                <label>
+                    Username
+                    <input
+                    className='username-input form-input'
+                    type="text"
+                    value={username}
+                    placeholder='Username'
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    />
+                </label>
+            </div>
+            <div  className='form-input'>
+                <label>
+                    Password
+                    <input
+                    className='form-input'
+                    type="password"
+                    value={password}
+                    placeholder='Password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    />
+                </label>
+            </div>
+            <div className='form-input'>
+                <label>
+                    Confirm Password
+                    <input
+                    className='form-input'
+                    type="password"
+                    value={confirmPassword}
+                    placeholder='Confirm Password'
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                    />
+                </label>
+            </div>
+            <button className='submit-sign-up form-input' type="submit">Sign Up</button>
+            </form>
         </div>
-        <button type="submit">Sign Up</button>
-        </form>
       </div>
   );
 }
